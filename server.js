b862e5c8c3e692ef39282750f7d1bedc0eb13f9d
Halina Mazur <mazurhalinapost@gmail.com>
2017-03-22 08:21:52 +0000

    correction server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(bodyparser.json());
 
 app.get('/', function (req, res) {
-  res.send('You can pass a string as a parameter (example: "January 1, 2016" or "1451606400")');
+  res.send('You can pass a string as a parameter (example: "January 1, 2016" or "1451606400"), and it will check to see whether that string contains either a unix timestamp or a natural language date');
 })
 
 app.get('/:date', function(req, res, next) {
@@ -19,12 +19,19 @@ app.get('/:date', function(req, res, next) {
         // unix
         unix = date;
         var temp = new Date(date * 1000);
-        natural =months[temp.getMonth()] + ' ' + temp.getDate() + ', ' + temp.getFullYear();
+        natural = months[temp.getMonth()] + ' ' + temp.getDate() + ', ' + temp.getFullYear();
     } else {
-        //natular
+        //string
         var temp = new Date(date);
-        natural = date;
-        unix = temp.getTime()/ 1000;
+        if(String(temp) != 'Invalid Date') { 
+            // date
+            natural = months[temp.getMonth()] + ' ' + temp.getDate() + ', ' + temp.getFullYear();
+            unix = temp.getTime()/ 1000;
+        } else {
+            // undefined
+            natural = null;
+            unix = null;
+        }
     }
     res.json({"unix": unix, "natural": natural});
 })
